660c1a7eaf9e6a50d7e1eaf46c31f36ff86b0b4f
Logan McNatt <logan.m.mcnatt@gmail.com>
2024-01-17 15:04:20 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Mission_02;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Rolling roll = new Rolling();
+        int numDice = 0;
+        int[] diceRolls = new int[11];
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n");
+        System.Console.WriteLine("How many dice rolls would you like to simulate? ");
+        numDice = Int32.Parse(System.Console.ReadLine());
+
+        roll.RollDice(numDice, diceRolls);
+
+        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS\r\n" +
+                                 "Each \"*\" represents 1% of the total number of rolls.\r\n" +
+                                 "Total number of rolls = " + numDice + ".\n");
+
+        for (int i = 0; i < diceRolls.Length; i++)
+        {
+            int pctRolls = (diceRolls[i] * 100) / numDice;
+            string pctAsterisks = "";
+            for (int j = 0; j < pctRolls; j++)
+            {
+                pctAsterisks += "*";
+            }
+            System.Console.WriteLine($"{i + 2}: {pctAsterisks}");
+        }
+
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
---
+++ Rolling.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_02
+{
+    internal class Rolling()
+    {
+        public void RollDice(int numDice, int[] dice)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < numDice; i++)
+            {
+                int dieOneNum = rnd.Next(6) + 1; // Returns random number from 1 to 6
+                int dieTwoNum = rnd.Next(6) + 1;
+                int diceNums = dieOneNum + dieTwoNum;
+
+                dice[diceNums - 2] += 1;
+            }
+        }
+    }
+}
