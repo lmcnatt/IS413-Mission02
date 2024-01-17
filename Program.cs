5ad406b0144350481bc1f671ff9f1e940b703d59
Logan McNatt <logan.m.mcnatt@gmail.com>
2024-01-17 15:22:18 -0700

    Added comments

--- Program.cs
+++ Program.cs
@@ -4,26 +4,28 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //Create new Rolling instance
+        // Create new Rolling instance and declare variables
         Rolling roll = new Rolling();
         int numDice = 0;
         int[] diceRolls = new int[11];
 
         System.Console.WriteLine("Welcome to the dice throwing simulator!\n");
         System.Console.WriteLine("How many dice rolls would you like to simulate? ");
-        numDice = Int32.Parse(System.Console.ReadLine());
+        numDice = Int32.Parse(System.Console.ReadLine()); // Get user input for number of dice to use
 
+        // Passing in the number of dice and the diceRolls array, call the rollDice "function" from the Rolling class
         roll.RollDice(numDice, diceRolls);
 
         System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS\r\n" +
                                  "Each \"*\" represents 1% of the total number of rolls.\r\n" +
                                  "Total number of rolls = " + numDice + ".\n");
 
+        // Write the results of RollDice
         for (int i = 0; i < diceRolls.Length; i++)
         {
             int pctRolls = (diceRolls[i] * 100) / numDice;
             string pctAsterisks = "";
-            for (int j = 0; j < pctRolls; j++)
+            for (int j = 0; j < pctRolls; j++) // Print an asterisk for every percent of rolls this number contains
             {
                 pctAsterisks += "*";
             }
--- Rolling.cs
+++ Rolling.cs
@@ -12,13 +12,14 @@ namespace Mission_02
         {
             Random rnd = new Random();
 
+            // Generate 2 random numbers and add them together for every die
             for (int i = 0; i < numDice; i++)
             {
                 int dieOneNum = rnd.Next(6) + 1; // Returns random number from 1 to 6
                 int dieTwoNum = rnd.Next(6) + 1;
-                int diceNums = dieOneNum + dieTwoNum;
+                int diceNums = dieOneNum + dieTwoNum; // Add both dice together
 
-                dice[diceNums - 2] += 1;
+                dice[diceNums - 2] += 1; // Add 1 roll to the total for this number
             }
         }
     }
